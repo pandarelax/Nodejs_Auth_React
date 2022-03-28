5b03c9b9b3de15b04e4461428ed4098ad41883fb
Efehan <efehanturhan@gmail.com>
2022-03-28 23:22:29 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+require("dotenv").config({ path: "./config.env" });
+const express = require("express");
+const app = express();
+const connectDB = require("./config/db");
+const errorHandler = require("./middleware/error");
+
+// Connect DB
+connectDB();
+
+app.use(express.json());
+
+app.get("/", (res, req, next) => {
+  res.send("Api running");
+});
+
+// Route connection
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/private", require("./routes/private"));
+
+// Error handler should be last piece of middleware
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+const server = app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
+
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Logged Error: ${err.message}`);
+  server.close(() => process.exit(1));
+});
